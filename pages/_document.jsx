2077bf17b3d836fcec3873c4ada72cbc2929f2b3
nphuhuy <phuhuy.social@gmail.com>
2025-03-19 06:19:04 +0700

    Portfolio

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
                 <Head>
                     <noscript>
                         <img height="1" width="1" style={{ display: 'none' }}
-                        src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`} />
+                        src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}/>
                     </noscript>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
